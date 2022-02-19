75d48c1e3d924a1786615d13d10c9e89deeab9d2
Michael Georgoulopoulos <mgeorgoulopoulos@gmail.com>
2022-02-19 19:38:24 +0200

    Add headers to make it easier to search text in VS. Also add StringConstants.cpp to get rid of redefinitions (header text is included, remember?)

---
+++ src/app/StringConstants.cpp
@@ -0,0 +1,33 @@
+#include "StringConstants.h"
+
+const char *kaverage = "average";
+const char *kboth = "both";
+const char *kChromosome = "Chromosome";
+const char *kEnd = "End";
+const char *kentropy = "entropy";
+const char *kGene = "Gene";
+const char *khigh = "high";
+const char *kkey = "key";
+const char *klist = "list";
+const char *klow = "low";
+const char *kmatrix = "matrix";
+const char *kmetric = "metric";
+const char *kmin_genes = "min-genes";
+const char *kmodel = "model";
+const char *kp_samples = "p-samples";
+const char *ksamples = "samples";
+const char *ksignal = "signal";
+const char *ksignal_type = "signal-type";
+const char *kStart = "Start";
+const char *kstats_output = "stats-output";
+const char *ktail = "tail";
+const char *koutput = "output";
+const char *koverlap_threshold = "overlap-threshold";
+const char *kpadj_threshold = "padj-threshold";
+const char *kradius = "radius";
+const char *ksettings = "settings";
+const char *kValue = "Value";
+const char *kx = "x";
+const char *ky = "y";
+const char *kz = "z";
+
--- src/app/StringConstants.h
+++ src/app/StringConstants.h
@@ -3,34 +3,35 @@
 
 #include <string>
 
-constexpr char *kaverage = "average";
-constexpr char *kboth = "both";
-constexpr char *kChromosome = "Chromosome";
-constexpr char *kEnd  = "End";
-constexpr char *kGene = "Gene";
-constexpr char *khigh = "high";
-constexpr char *kkey = "key";
-constexpr char *klist = "list";
-constexpr char *klow = "low";
-constexpr char *kmatrix = "matrix";
-constexpr char *kmetric = "metric";
-constexpr char *kmin_genes = "min-genes";
-constexpr char *kmodel = "model";
-constexpr char *kp_samples = "p-samples";
-constexpr char *ksamples = "samples";
-constexpr char *ksignal = "signal";
-constexpr char *ksignal_type = "signal-type";
-constexpr char *kStart = "Start";
-constexpr char *kstats_output= "stats-output";
-constexpr char *ktail = "tail";
-constexpr char *koutput = "output";
-constexpr char *koverlap_threshold = "overlap-threshold";
-constexpr char *kpadj_threshold = "padj-threshold";
-constexpr char *kradius = "radius";
-constexpr char *ksettings = "settings";
-constexpr char *kValue = "Value";
-constexpr char *kx = "x";
-constexpr char *ky = "y";
-constexpr char *kz = "z";
+extern const char *kaverage;
+extern const char *kboth;
+extern const char *kChromosome;
+extern const char *kEnd;
+extern const char *kentropy;
+extern const char *kGene;
+extern const char *khigh;
+extern const char *kkey;
+extern const char *klist;
+extern const char *klow;
+extern const char *kmatrix;
+extern const char *kmetric;
+extern const char *kmin_genes;
+extern const char *kmodel;
+extern const char *kp_samples;
+extern const char *ksamples;
+extern const char *ksignal;
+extern const char *ksignal_type;
+extern const char *kStart;
+extern const char *kstats_output;
+extern const char *ktail;
+extern const char *koutput;
+extern const char *koverlap_threshold;
+extern const char *kpadj_threshold;
+extern const char *kradius;
+extern const char *ksettings;
+extern const char *kValue;
+extern const char *kx;
+extern const char *ky;
+extern const char *kz;
 
 #endif // __STRING_CONSTANTS_H__
